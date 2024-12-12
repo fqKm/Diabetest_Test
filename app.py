8d68ae1f738d13f944769bb59f1b8de6764dd4f0
Faiq Khoirul Muna <faiqkhoirul04@gmail.com>
2024-12-12 19:45:09 +0700

    update accuracy

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html',probabilities=None, prediction=None, data=None)
 
 @app.route('/post', methods=['POST'])
 def predict():
@@ -15,8 +15,9 @@ def predict():
     df_input = pd.DataFrame([input_data])
     rfc = pickle.load(open('model.pkl', 'rb'))
     result = rfc.predict(df_input)
-    return render_template('index.html', prediction=result, data=input_data)
+    probab = rfc.predict_proba(df_input)[0]
+    probability_dictionary = {label: round(probability * 100, 2) for label, probability in zip(rfc.classes_, probab)}
+    return render_template('index.html', prediction=result, probabilities=probability_dictionary, data=input_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
-#%%
